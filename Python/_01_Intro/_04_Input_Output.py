7fe85e161586fdfe7f4b5dfc8827afeadde0baba
Jose Santos <airamez@gmail.com>
2024-07-24 22:17:18 -0700

    Adding the Input/Output content

---
+++ Python/_01_Intro/_04_Input_Output.py
@@ -0,0 +1,41 @@
+# Input: read - reads a value from console and returns the value as string
+# Output: print - prints to the console
+
+name = input("What is your name: ")
+print("Hello", name)
+print(f"Hello {name}")
+greatings = f"Hello {name}" 
+print(greatings)
+
+# WARNING: Remember to show some reading of invalid types
+
+n1 = input ("Integer 1: ") # Returns as a String
+n1 = int(input("Integer number 1: ")) # Convert from string to integer
+n2 = int(input("Integer number 2: "))
+average = (n1 + n2) / 2
+print(f"The average of {n1} and {n2} is {average}")
+
+n1 = float(input("Float number 1: "))
+n2 = float(input("Float number 2: "))
+average = (n1 + n2) / 2
+print(f"The average of {n1} and {n2} is {average}")
+
+is_married = bool(input("Are you married?"))
+
+# print() always add a new line
+# To avoid the new line we must to indicate that the end of the string is a empty string
+print("Jose ", end="")
+print("Santos")
+
+# Multiple outputs will be sepated by spaces (We already used this a lot)
+first_name = input("First name: ")
+last_name = input("Last name: ")
+middle_name = input("Middle name: ")
+print(first_name, middle_name, last_name)
+
+# Using interpolation
+print(f"{first_name} {middle_name} {last_name}")
+
+# Storing in a variable
+full_name = f"{first_name} {middle_name} {last_name}"
+print(f"Full name: {full_name}")
---
+++ Python/_01_Intro/_05_Input_Output_Practices.py
@@ -0,0 +1,27 @@
+"""
+Create a python program to:
+
+# read the part of a phone number and print the full number using the format below
+  COUNTRY_CODE (AREA_CODE) PREFIX-LINE_NUMBER
+  Ex: +1 (640) 850-1189
+
+# read 3 numbers and print the sum and average
+
+# read the day, month and year and print in the format below
+  MONTH/DAY/YEAR
+
+# read five numbers and print them using the format below
+  [NUMBER_1, NUMBER_2, NUMBER_3, NUMBER_4]
+
+# read the id, description, price and quantity of a product
+  and print the content using the format below:
+  {id: [ID], description: [DESCRIPTION], price: [PRICE], quantity: [QUANTITY], total: [PRICE * QUANTITY]}
+  Example:
+  INPUT
+    ID: 123
+    Description: Python Programming Book
+    Price: 34.75
+    Quantity: 3
+  OUTPUT
+    {id: [123], description: [Python Programming Book], price: [34.75], quantity: 3, total: 104.25}
+"""
