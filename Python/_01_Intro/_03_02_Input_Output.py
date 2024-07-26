b7b110c9dfe533e94ca357d314d97b7e84b18219
Jose Santos <airamez@gmail.com>
2024-07-25 21:02:55 -0700

    Class #4: Input & Output

--- Python/_01_Intro/_03_02_Input_Output.py
+++ Python/_01_Intro/_03_02_Input_Output.py
@@ -2,12 +2,15 @@
 # Output: print - prints to the console
 
 name = input("What is your name: ")
+
 print("Hello", name)
+
 print(f"Hello {name}")
+
 greatings = f"Hello {name}" 
 print(greatings)
 
-# WARNING: Remember to show some reading of invalid types
+# WARNING: Remember to show some input of invalid types
 
 n1 = input ("Integer 1: ") # Returns as a String
 n1 = int(input("Integer number 1: ")) # Convert from string to integer
@@ -20,11 +23,12 @@ n2 = float(input("Float number 2: "))
 average = (n1 + n2) / 2
 print(f"The average of {n1} and {n2} is {average}")
 
-is_married = bool(input("Are you married?"))
+is_married = bool(input("Are you married? ")) # bool() only return false if the input is empty
+print(is_married)
 
 # print() always add a new line
 # To avoid the new line we must to indicate that the end of the string is a empty string
-print("Jose ", end="")
+print("Jose ", end="") # usuful when using looping
 print("Santos")
 
 # Multiple outputs will be sepated by spaces (We already used this a lot)
