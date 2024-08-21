05daae10d5fd9906c8bf3f2bd15049cf387c5e1c
Jose Santos <airamez@gmail.com>
2024-08-20 21:37:31 -0700

    Class #11: Basic Operations Practices (Question 11)

--- Python/_01_Intro/_06_Practices_11.py
+++ Python/_01_Intro/_06_Practices_11.py
@@ -1,4 +1,12 @@
 """
-11. Read the buying price (cost), the desired profit percentage and
-    calculate the sales price of the product.
-    """
+11. Read the buying price (cost), and desired profit percentage and calculate the 
+    sales price of the product.
+"""
+
+buy_price = float(input("Buy Price: "))
+desired_profit_percentage = float(input("Desired profit %: "))
+
+profit = buy_price * desired_profit_percentage / 100
+sales_price = buy_price + profit
+
+print(f"Sale price = {round(sales_price, 2)}; Profit: {round(profit, 2)}")
