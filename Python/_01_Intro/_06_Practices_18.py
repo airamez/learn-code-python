dee5a66ed3377077e7c0a8fda51d276d5c97d6ec
Jose Santos <airamez@gmail.com>
2024-09-10 20:34:33 -0700

    Class #16: Basic Operations Practices (Question 18)

--- Python/_01_Intro/_06_Practices_18.py
+++ Python/_01_Intro/_06_Practices_18.py
@@ -1,5 +1,40 @@
 """
-18. Read an integer number with 5 digits and generate a 
-    new number with the digits in reverse order
+18. Read an integer number with 5 digits and
+    generate a new number with the digits in
+    reverse order
+    
+    Examples:
+    12345 => 54321
+    54321 => 12345
+    33666 => 66633
+    00001 => 10000
+    00012 => 21000
+    00123 => 32100
+    10000 => 1
+    90000 => 9
 """
 
+number = int(input("Number: "))
+
+inversed_number = 0
+
+last_digit = number % 10
+inversed_number = last_digit
+
+number = number // 10
+last_digit = number % 10
+inversed_number = inversed_number * 10 + last_digit
+
+number = number // 10
+last_digit = number % 10
+inversed_number = inversed_number * 10 + last_digit
+
+number = number // 10
+last_digit = number % 10
+inversed_number = inversed_number * 10 + last_digit
+
+number = number // 10
+last_digit = number % 10
+inversed_number = inversed_number * 10 + last_digit
+
+print(f"Inversed Number = {inversed_number}")
